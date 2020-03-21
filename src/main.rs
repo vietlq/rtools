fb074875a14440a8764aaacfc0dff7c7cd03d075
Viet Le <vietlq85@gmail.com>
2020-03-21 18:47:16 +0000

    handle_pos_parse

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,13 @@ use regex::Regex;
 
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 
+fn handle_pos_parse(s: &str) -> u32 {
+    match s.parse::<u32>() {
+        Ok(n) => n,
+        Err(_) => std::u32::MAX
+    }
+}
+
 fn main() {
     let matches = App::new("rcut")
         .version(VERSION)
@@ -25,7 +32,7 @@ fn main() {
         .get_matches();
     let characters = matches.value_of("characters").unwrap();
 
-    let char_regex = Regex::new(r"^\d+(\-\d+)?(,\d+(\-\d+)?)?$").unwrap();
+    let char_regex = Regex::new(r"^\d+(\-(\d+)?)?(,\d+(\-(\d+)?)?)?$").unwrap();
     if char_regex.is_match(characters) {
         println!("Good list of characters :)");
     } else {
@@ -35,7 +42,7 @@ fn main() {
 
     for char_part in characters.split(",") {
         let positions: Vec<u32> = char_part.split("-")
-            .map(|s| s.parse::<u32>().unwrap()).collect();
+            .map(|s| handle_pos_parse(s)).collect();
         println!("char_part = {}", char_part);
         for pos in positions {
             println!("pos = {}", pos);
