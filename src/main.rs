8673a824549a66fba9fc6856e32f8807cb05610d
Viet Le <vietlq85@gmail.com>
2020-03-21 17:44:35 +0000

    first arg c

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+extern crate clap;
+use clap::{App, Arg, ArgGroup, SubCommand};
+
+const VERSION: &'static str = env!("CARGO_PKG_VERSION");
+
 fn main() {
-    println!("Hello, world!");
+    let matches = App::new("rcut")
+        .version(VERSION)
+        .arg(
+            Arg::with_name("characters")
+                .short("c")
+                .long("characters")
+                .value_name("LIST")
+                .help("select only these characters"),
+        )
+        .get_matches();
+    let characters = matches.value_of("characters").unwrap();
+    println!("characters = {}", characters);
 }
