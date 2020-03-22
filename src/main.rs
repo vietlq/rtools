a24eadd1874941aa4d397385ac8d2af585849455
Viet Le <vietlq85@gmail.com>
2020-03-22 11:33:53 +0000

    trimmed code and fixed regex

--- src/main.rs
+++ src/main.rs
@@ -18,10 +18,6 @@ fn handle_pos_parse(s: &str) -> u32 {
 
 fn char_part_to_pair(char_part: &str) -> (u32, u32) {
     let positions: Vec<u32> = char_part.split("-").map(|s| handle_pos_parse(s)).collect();
-    println!("char_part = {}", char_part);
-    for pos in &positions {
-        println!("pos = {}", pos);
-    }
     match positions.len() {
         1 => (positions[0], positions[0]),
         2 => (positions[0], positions[1]),
@@ -45,16 +41,18 @@ fn main() {
         .get_matches();
     let characters = matches.value_of("characters").unwrap();
 
-    let char_regex = Regex::new(r"^\d+(\-(\d+)?)?(,\d+(\-(\d+)?)?)?$").unwrap();
-    if char_regex.is_match(characters) {
-        println!("Good list of characters :)");
-    } else {
-        println!("Bad list of characters!");
+    let char_regex = Regex::new(r"^\d+(\-(\d+)?)?(,\d+(\-(\d+)?)?)*$").unwrap();
+    if !char_regex.is_match(characters) {
+        println!("Bad input for the flag -c");
         process::exit(1);
     }
 
-    for char_part in characters.split(",") {
-        let (p1, p2) = char_part_to_pair(char_part);
-        println!("pair = ({}, {})", p1, p2);
+    let mut char_pairs: Vec<(u32, u32)> = characters
+        .split(",")
+        .map(|char_part| char_part_to_pair(char_part))
+        .collect();
+    char_pairs.sort();
+    for char_pair in char_pairs {
+        println!("char_pair = ({}, {})", char_pair.0, char_pair.1);
     }
 }
